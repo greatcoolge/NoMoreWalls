b13267cf3a2ad2d284e09d19d3c03f5fe8f5362f
greatcoolge <121862222+greatcoolge@users.noreply.github.com>
2025-07-14 16:12:37 +0800

    dynamic.py

--- dynamic.py
+++ dynamic.py
@@ -95,7 +95,7 @@ def peasoft():
     return session.get("https://gist.githubusercontent.com/peasoft/8a0613b7a2be881d1b793a6bb7536281/raw/417c1d6a75a53d6c197448762e7c97852d34787f/-").text
 
 AUTOURLS = []
-AUTOFETCH = [vpn_fail, sharkdoor, kkzui]
+AUTOFETCH = [vpn_fail, sharkdoor, fetch_cfmem_latest_v2rayse]
 
 if __name__ == '__main__':
     print("URL 抓取："+', '.join([_.__name__ for _ in AUTOURLS]))
